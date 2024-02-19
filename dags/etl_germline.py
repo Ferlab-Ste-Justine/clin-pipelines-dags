a1dc784fe55a4343702587304aff5dc2ea381eaf
Laura Bégin <lbegin@ferlab.bio>
2024-02-19 15:37:57 -0500

    feat: CLIN-2594 Always run all QA tests

--- dags/etl_germline.py
+++ dags/etl_germline.py
@@ -10,8 +10,9 @@ from lib.groups.index.index import index
 from lib.groups.ingest.ingest_germline import ingest_germline
 from lib.groups.index.prepare_index import prepare_index
 from lib.groups.index.publish_index import publish_index
+from lib.groups.qa import qa
 from lib.slack import Slack
-from lib.tasks import (enrich, qa)
+from lib.tasks import enrich
 from lib.tasks.notify import notify
 from lib.tasks.params_validate import validate_release_color
 from lib.utils_etl import color, batch_id, skip_import, skip_batch, default_or_initial, skip_notify
@@ -71,27 +72,10 @@ with DAG(
         spark_jar=spark_jar()
     )
 
-
-    @task_group(group_id='qa')
-    def qa_germline():
-        non_empty_tables = qa.non_empty_tables(release_id(), spark_jar())
-        no_dup_gnomad = qa.no_dup_gnomad(release_id(), spark_jar())
-        no_dup_nor_snv = qa.no_dup_nor_snv(release_id(), spark_jar())
-        no_dup_nor_consequences = qa.no_dup_nor_consequences(release_id(), spark_jar())
-        no_dup_nor_variants = qa.no_dup_nor_variants(release_id(), spark_jar())
-        no_dup_snv = qa.no_dup_snv(release_id(), spark_jar())
-        no_dup_consequences = qa.no_dup_consequences(release_id(), spark_jar())
-        no_dup_variants = qa.no_dup_variants(release_id(), spark_jar())
-        no_dup_variant_centric = qa.no_dup_variant_centric(release_id(), spark_jar())
-        no_dup_cnv_centric = qa.no_dup_cnv_centric(release_id(), spark_jar())
-        same_list_nor_snv_nor_variants = qa.same_list_nor_snv_nor_variants(release_id(), spark_jar())
-        same_list_snv_variants = qa.same_list_snv_variants(release_id(), spark_jar())
-        same_list_variants_variant_centric = qa.same_list_variants_variant_centric(release_id(), spark_jar())
-
-        [non_empty_tables, no_dup_gnomad, no_dup_nor_snv, no_dup_nor_consequences, no_dup_nor_variants, no_dup_snv,
-         no_dup_consequences, no_dup_variants, no_dup_variant_centric, no_dup_cnv_centric,
-         same_list_nor_snv_nor_variants, same_list_snv_variants, same_list_variants_variant_centric]
-
+    qa_group = qa(
+        release_id=release_id(),
+        spark_jar=spark_jar()
+    )
 
     index_group = index(
         release_id=release_id(),
@@ -111,4 +95,4 @@ with DAG(
         skip=skip_notify()
     )
 
-    params_validate_task >> ingest_germline_group >> enrich_germline() >> prepare_group >> qa_germline() >> index_group >> publish_group >> notify_task
+    params_validate_task >> ingest_germline_group >> enrich_germline() >> prepare_group >> qa_group >> index_group >> publish_group >> notify_task
