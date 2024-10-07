e84147f8e6038b669729c08a7e5a2633f82b5718
LysianeBouchard <lysiane.bouchard.hsj@ssss.gouv.qc.ca>
2024-10-07 09:32:36 -0400

    feat: CLIN-3267 First draft
    Temporary commit to avoid losing work locally.
    
    This code works locally with minikube for test_nextflow_operator and etl_variant_annotation.
    It is not tested on etl.py.
    
    It reuse a configuration mechanism similar as in cqdg
    Key modifications in comparison to cqdg:
    - service_account_name specified in BaseConfig instead KubeConfig
    - shallow copy in build_operator and partial methods in BaseConfig
    - BaseConfig.args renamed to BaseConfig.append_args
    
    There are a few todos in the code to address.

---
+++ dags/lib/config_nextflow_pipelines.py
@@ -0,0 +1,55 @@
+"""
+This module contains configuration for nextflow pipelines
+and their associated kubernetes config maps.
+
+Centralizing the configuration of kubernetes config maps will
+ease consistency in the nextflow long running pod that is used
+for troubleshooting.  This pod is not managed by airflow
+and is defined in another repository. All the nextflow config
+maps must be mounted in this pod.
+
+Centralization helps identify required config maps and their mount
+paths. It also prevents mount path conflicts. Always use a unique
+mount path for each config map.
+"""
+from lib.operators.base_kubernetes import ConfigMap
+from lib.operators.nextflow import NextflowPipeline
+
+
+####################################
+# Define nextflow config maps here #
+####################################
+
+
+default_config_map = ConfigMap(
+    name='nextflow',
+    mount_path='/root/nextflow/config'
+)
+default_config_file = f"{default_config_map.mount_path}/nextflow.config"
+
+variant_annotation_config_map = ConfigMap(
+    name='nextflow-variant-annotation',
+    mount_path='/root/nextflow/variant_annotation'
+)
+
+
+##################################
+# Define nextflow pipelines here #
+##################################
+
+
+base_nextflow_pipeline = NextflowPipeline(
+    config_files=[default_config_file],
+    config_maps=[default_config_map]
+)
+
+variant_annotation_pipeline = base_nextflow_pipeline \
+    .with_url('Ferlab-Ste-Justine/Post-processing-Pipeline') \
+    .with_revision('v2.1.0') \
+    .append_config_maps(variant_annotation_config_map) \
+    .append_config_files(f'{variant_annotation_config_map.mount_path}/variant_annotation_v2.x.config') \
+    .with_params_file(f'{variant_annotation_config_map.mount_path}/params_v2.x.json')
+
+svclustering_pipeline = base_nextflow_pipeline \
+    .with_url('Ferlab-Ste-Justine/ferlab-svclustering-parental-origin') \
+    .with_revision('v1.1')
--- dags/test_nextflow_operator.py
+++ dags/test_nextflow_operator.py
@@ -4,8 +4,8 @@ from airflow import DAG
 from airflow.models.param import Param
 
 from lib import config
-from lib.config import env, Env, K8sContext
-from lib.operators.nextflow import NextflowOperator
+from lib.config import env, Env, nextflow_base_config
+from lib.config_nextflow_pipelines import default_config_file as default_nextflow_config_file
 from lib.slack import Slack
 
 DEFAULT_NEXTFLOW_COMMAND = [
@@ -13,7 +13,7 @@ DEFAULT_NEXTFLOW_COMMAND = [
     "run",
     "nextflow-io/hello",
     "-c",
-    "/root/nextflow/config/nextflow.config"
+    default_nextflow_config_file
 ]
 if (config.show_test_dags or env in [Env.QA, Env.STAGING]):
     with DAG(
@@ -30,26 +30,25 @@ if (config.show_test_dags or env in [Env.QA, Env.STAGING]):
                 be passed to the bash shell in the nextflow pod. Here each
                 line represent a separate argument.
 
-                Here the `/root/nextflow/config/nextflow.config` file is a
+                Here the `{default_nextflow_config_file}` file is a
                 valid nextflow configuration file that will be available in
                 the pod. It contains configuration settings specific to the
                 Qlin execution environment. You should always include it in
                 your nextflow command, unless you have a valid reason not to.
 
                 The nextflow working directory is set to
-                `{config.nextflow_working_dir}`. You can overwrite it via the
-                `-work-dir` option, but please use a location under this
-                directory.
+                `{nextflow_base_config.nextflow_working_dir}`. You can
+                overwrite it via the `-work-dir` option, but please use a
+                location under this directory.
                 """
             )
         },
         render_template_as_native_obj=True
     ) as dag:
 
-        NextflowOperator(
+        nextflow_base_config.operator(
             task_id='test_nextflow_operator',
-            name="test_nextflow_operator",  # prefix for the pod name
-            k8s_context=K8sContext.ETL,
+            name="test_nextflow_operator",
             arguments="{{ params.nextflow_command }}",
             on_execute_callback=Slack.notify_dag_start,
             on_success_callback=Slack.notify_dag_completion,
