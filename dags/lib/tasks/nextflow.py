d9d5a344531f79d08be56e6685b48aeb6333400c
Laura Bégin <lbegin@ferlab.bio>
2024-10-10 16:32:01 -0400

    :recycle: refactor: CLIN-3267 Code review suggestion

--- dags/lib/tasks/nextflow.py
+++ dags/lib/tasks/nextflow.py
@@ -1,18 +1,11 @@
 from typing import List
 
 from airflow.exceptions import AirflowSkipException
+from airflow.models import MappedOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.utils.context import Context
 
-from lib.config import (
-    clin_datalake_bucket,
-    s3_conn_id,
-    nextflow_base_config
-)
-from lib.config_nextflow_pipelines import (
-    NextflowPipeline,
-    svclustering_pipeline
-)
+from lib.config import (clin_datalake_bucket, s3_conn_id, nextflow_base_config)
 from lib.operators.nextflow import NextflowOperator
 from lib.operators.spark_etl import SparkETLOperator
 from lib.utils_etl import ClinAnalysis
@@ -21,7 +14,7 @@ from lib.utils_etl import ClinAnalysis
 def prepare_svclustering_parental_origin(
         batch_ids: List[str],
         spark_jar: str,
-        skip: str = '') -> SparkETLOperator:
+        skip: str = '') -> MappedOperator:
     return SparkETLOperator.partial(
         task_id='prepare_svclustering_parental_origin',
         name='prepare-svclustering-parental-origin',
@@ -60,7 +53,7 @@ def svclustering_parental_origin(batch_ids: List[str], skip: str = ''):
                 '--outdir', self.output_key
             ]
 
-        def execute(self, context: Context, **kwargs):
+        def execute(self, context: Context):
             batch_type = context['ti'].xcom_pull(
                 task_ids='detect_batch_type',
                 key=self.batch_id
@@ -73,33 +66,18 @@ def svclustering_parental_origin(batch_ids: List[str], skip: str = ''):
             if not s3.check_for_key(self.input_key):
                 raise AirflowSkipException(f'No CSV input file for batch id \'{self.batch_id}\'')
 
-            super().execute(context, **kwargs)
+            super().execute(context)
 
-    return nextflow_base_config\
+    return nextflow_base_config \
+        .with_pipeline('Ferlab-Ste-Justine/ferlab-svclustering-parental-origin') \
+        .with_revision('v1.1.1-clin') \
         .append_args(
-            *get_run_pipeline_arguments(svclustering_pipeline),
-            '--fasta',  f's3://{clin_datalake_bucket}/public/refgenomes/hg38/Homo_sapiens_assembly38.fasta',
+            '--fasta', f's3://{clin_datalake_bucket}/public/refgenomes/hg38/Homo_sapiens_assembly38.fasta',
             '--fasta_fai', f's3://{clin_datalake_bucket}/public/refgenomes/hg38/Homo_sapiens_assembly38.fasta.fai',
-            '--fasta_dict', f's3://{clin_datalake_bucket}/public/refgenomes/hg38/Homo_sapiens_assembly38.dict'
-        ) \
-        .with_config_maps(svclustering_pipeline.config_maps) \
+            '--fasta_dict', f's3://{clin_datalake_bucket}/public/refgenomes/hg38/Homo_sapiens_assembly38.dict') \
         .partial(
             SVClusteringParentalOrigin,
             task_id='svclustering_parental_origin',
             name='svclustering_parental_origin',
             skip=skip
         ).expand(batch_id=batch_ids)
-
-
-def get_run_pipeline_arguments(pipeline: NextflowPipeline) -> List[str]:
-    new_args = ['nextflow']
-
-    for config_file in pipeline.config_files:
-        new_args.extend(["-c", config_file])
-
-    new_args.extend(["run", pipeline.url, "-r", pipeline.revision])
-
-    if (pipeline.params_file):
-        new_args.extend(["-params-file", pipeline.params_file])
-
-    return new_args
