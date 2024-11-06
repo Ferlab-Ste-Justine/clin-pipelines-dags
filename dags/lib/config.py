a353cfb87e9d2256b8bdb2fa8e57c55b407507fe
Yann MARCOU <creativeyann17@gmail.com>
2024-11-06 11:58:33 -0500

    feat: CLIN-3425 - release

--- dags/lib/config.py
+++ dags/lib/config.py
@@ -1,8 +1,7 @@
 import kubernetes
 from airflow.exceptions import AirflowConfigException
 from airflow.models import Variable
-
-from lib.operators.base_kubernetes import KubeConfig, ConfigMap
+from lib.operators.base_kubernetes import ConfigMap, KubeConfig
 from lib.operators.nextflow import NextflowOperatorConfig
 
 
@@ -73,7 +72,7 @@ elif env == Env.STAGING:
     pipeline_image = 'ferlabcrsj/clin-pipelines:b4b2ba4'
     panels_image = 'ferlabcrsj/clin-panels:13b8182d493658f2c6e0583bc275ba26967667ab-1683653903'
     es_url = 'http://elasticsearch:9200'
-    spark_jar = 'clin-variant-etl-v3.4.2_fix_nested_and_clinvar.jar'
+    spark_jar = 'clin-variant-etl-v3.5.1.jar'
     obo_parser_spark_jar = 'obo-parser-v1.1.0.jar'
     ca_certificates = 'ingress-ca-certificate'
     minio_certificate = 'minio-ca-certificate'
@@ -99,7 +98,7 @@ elif env == Env.PROD:
     pipeline_image = 'ferlabcrsj/clin-pipelines:b4b2ba4'
     panels_image = 'ferlabcrsj/clin-panels:13b8182d493658f2c6e0583bc275ba26967667ab-1683653903'
     es_url = 'https://workers.search.cqgc.hsj.rtss.qc.ca:9200'
-    spark_jar = 'clin-variant-etl-v3.4.2_fix_nested_and_clinvar.jar'
+    spark_jar = 'clin-variant-etl-v3.5.1.jar'
     obo_parser_spark_jar = 'obo-parser-v1.1.0.jar'
     ca_certificates = 'ca-certificates-bundle'
     minio_certificate = 'ca-certificates-bundle'
