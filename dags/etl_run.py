49d99b8f95004434f18daa8b7ab762e4b7a5d6da
Laura Bégin <laura.begin.hsj@ssss.gouv.qc.ca>
2025-02-18 10:44:39 -0500

    refactor: CLIN-3728 Use TaskFlow + call run after get_all_sequencing_ids

--- dags/etl_run.py
+++ dags/etl_run.py
@@ -6,7 +6,6 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.models.param import Param
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
 from airflow.utils.trigger_rule import TriggerRule
 from pandas import DataFrame
 
@@ -14,27 +13,24 @@ from lib.datasets import enriched_clinical
 from lib.slack import Slack
 
 with DAG(
-    dag_id='etl_run',
-    start_date=datetime(2022, 1, 1),
-    schedule_interval=None,
-    catchup=False,
-    params={
-        'sequencing_ids': Param([], type=['null', 'array']),
-    },
-    render_template_as_native_obj=True,
-    default_args={
-        'trigger_rule': TriggerRule.NONE_FAILED,
-        'on_failure_callback': Slack.notify_task_failure,
-    },
-    max_active_tasks=1,
-    max_active_runs=1
+        dag_id='etl_run',
+        start_date=datetime(2022, 1, 1),
+        schedule_interval=None,
+        catchup=False,
+        params={
+            'sequencing_ids': Param([], type=['null', 'array']),
+        },
+        render_template_as_native_obj=True,
+        default_args={
+            'trigger_rule': TriggerRule.NONE_FAILED,
+            'on_failure_callback': Slack.notify_task_failure,
+        },
+        max_active_tasks=1,
+        max_active_runs=1
 ) as dag:
-
     def sequencing_ids():
         return '{{ params.sequencing_ids }}'
 
-    def _run(sequencing_ids: List[str]):
-        logging.info(f'Run ETLs for total: {len(sequencing_ids)} sequencing_ids: {sequencing_ids}')
 
     start = EmptyOperator(
         task_id="start",
@@ -78,15 +74,17 @@ with DAG(
         return all_sequencing_ids
 
 
-    run_etl = PythonOperator(
-        task_id='run',
-        op_args=[sequencing_ids()],
-        python_callable=_run,
-    )
+    all_sequencing_ids = get_all_sequencing_ids(sequencing_ids())
+
+
+    @task(task_id="run")
+    def run(sequencing_ids: Set[str]):
+        logging.info(f'Run ETLs for total: {len(sequencing_ids)} sequencing_ids: {sequencing_ids}')
+
 
     slack = EmptyOperator(
         task_id="slack",
         on_success_callback=Slack.notify_dag_completion
     )
 
-    start >> run_etl >> get_all_sequencing_ids(sequencing_ids()) >> slack
+    start >> all_sequencing_ids >> run(all_sequencing_ids) >> slack
