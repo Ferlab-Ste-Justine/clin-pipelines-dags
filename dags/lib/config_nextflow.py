cba54812d0e3ffcccbcf9e1bb431e55b8fc936e5
LysianeBouchard <lysiane.bouchard.hsj@ssss.gouv.qc.ca>
2025-04-29 13:03:18 -0400

    feat:CLIN-4404-add-nextflow-task-for-cnv-post-processing-pipeline

--- dags/lib/config_nextflow.py
+++ dags/lib/config_nextflow.py
@@ -29,12 +29,21 @@ nextflow_post_processing_config_map = ConfigMap(
 nextflow_post_processing_config_file = f"{nextflow_post_processing_config_map.mount_path}/postprocessing.config"
 nextflow_post_processing_params_file = f"{nextflow_post_processing_config_map.mount_path}/postprocessing-params.json"
 
+nextflow_cnv_post_processing_config_map = ConfigMap(
+    name='nextflow-cnv-postprocessing',
+    mount_path='/root/nextflow/config/cnv-postprocessing'
+)
+nextflow_cnv_post_processing_config_file = f"{nextflow_cnv_post_processing_config_map.mount_path}/cnv-postprocessing.config"
+nextflow_cnv_post_processing_params_file = f"{nextflow_cnv_post_processing_config_map.mount_path}/cnv-postprocessing-params.json"
+
+
 ##################################
 # Define nextflow revisions here #
 ##################################
 nextflow_svclustering_revision = 'v1.3.1-clin'
 nextflow_svclustering_parental_origin_revision = 'v1.1.1-clin'
 nextflow_post_processing_revision = 'v2.8.0'
+nextflow_cnv_post_processing_revision = 'v1.0.0'
 
 #######################################
 # Define nextflow pipeline names here #
@@ -42,15 +51,23 @@ nextflow_post_processing_revision = 'v2.8.0'
 nextflow_svclustering_pipeline = 'Ferlab-Ste-Justine/ferlab-svclustering'
 nextflow_svclustering_parental_origin_pipeline = 'Ferlab-Ste-Justine/ferlab-svclustering-parental-origin'
 nextflow_post_processing_pipeline = 'Ferlab-Ste-Justine/Post-processing-Pipeline'
+nextflow_cnv_post_processing_pipeline = 'Ferlab-Ste-Justine/cnv-post-processing'
+
 
 #############################################################
 # Define nextflow input and output file configurations here #
 #############################################################
 nextflow_bucket = config.clin_datalake_bucket
 nextflow_exomiser_input_key = lambda analysis_id: f'nextflow/exomiser/input/{analysis_id}.pheno.json'
+
 nextflow_svclustering_parental_origin_input_key = lambda batch_id: \
     f'nextflow/svclustering_parental_origin_input/{batch_id}/{batch_id}.csv'
+
 nextflow_post_processing_input_key = lambda _hash: f'nextflow/post_processing/input/{_hash}.samplesheet.csv'
 nextflow_post_processing_info_output_key = lambda _hash: f'nextflow/post_processing/output/runs/{_hash}'  # _hash here can be a template
 nextflow_post_processing_vep_output_key = 'nextflow/post_processing/output/ensemblvep'
 nextflow_post_processing_exomiser_output_key = 'nextflow/post_processing/output/exomiser'
+
+nextflow_cnv_post_processing_input_key = lambda _hash: f'nextflow/cnv_post_processing/input/{_hash}.samplesheet.csv'
+nextflow_cnv_post_processing_info_output_key = lambda _hash: f'nextflow/cnv_post_processing/output/runs/{_hash}'  # _hash here can be a template
+nextflow_cnv_post_processing_exomiser_output_key = 'nextflow/cnv_post_processing/output/exomiser'
