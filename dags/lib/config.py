4bd3f2484fa17a8f370ce0e06f488c3523943eed
meek0 <cedricgfont@gmail.com>
2025-04-30 14:33:09 -0400

    feat: CLIN-4446 bump spark_jar

--- dags/lib/config.py
+++ dags/lib/config.py
@@ -113,7 +113,7 @@ elif env == Env.PROD:
     pipeline_image = 'ferlabcrsj/clin-pipelines:bb6aad0'
     panels_image = 'ferlabcrsj/clin-panels:13b8182d493658f2c6e0583bc275ba26967667ab-1683653903'
     es_url = 'https://workers.search.cqgc.hsj.rtss.qc.ca:9200'
-    spark_jar = 'clin-variant-etl-v3.8.10.jar'
+    spark_jar = 'clin-variant-etl-v3.9.1.jar'
     obo_parser_spark_jar = 'obo-parser-v1.1.0.jar'
     ca_certificates = 'ca-certificates-bundle'
     minio_certificate = 'ca-certificates-bundle'
