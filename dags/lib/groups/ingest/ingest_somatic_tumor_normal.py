00f807cc22d4b6ee97c177927bfda9541613759f
LysianeBouchard <lysiane.bouchard.hsj@ssss.gouv.qc.ca>
2025-08-19 01:40:13 -0400

    fix: CLIN-5015 take bioinfo analysis code into account when inferring batch ids from analysis ids in etl_ingest
    Without this fix, the dag fail for an analysis that appear in both tumor only and tumor normal batches.

--- dags/lib/groups/ingest/ingest_somatic_tumor_normal.py
+++ dags/lib/groups/ingest/ingest_somatic_tumor_normal.py
@@ -3,7 +3,7 @@ from lib.groups.ingest.ingest_fhir import ingest_fhir
 from lib.groups.normalize.normalize_somatic_tumor_normal import \
     normalize_somatic_tumor_normal
 from lib.tasks import batch_type, clinical
-from lib.utils_etl import ClinAnalysis
+from lib.utils_etl import BioinfoAnalysisCode, ClinAnalysis
 
 
 @task_group(group_id='ingest_somatic_tumor_normal')
@@ -40,7 +40,7 @@ def ingest_somatic_tumor_normal(
     )
 
     get_all_analysis_ids = clinical.get_all_analysis_ids(analysis_ids=analysis_ids, batch_id=batch_id, skip=skip_all)
-    get_analysis_ids_related_batch_task = clinical.get_analysis_ids_related_batch(analysis_ids=get_all_analysis_ids, batch_id=batch_id, skip=skip_all)
+    get_analysis_ids_related_batch_task = clinical.get_analysis_ids_related_batch(bioinfo_analysis_code=BioinfoAnalysisCode.TNEBA, analysis_ids=get_all_analysis_ids, batch_id=batch_id, skip=skip_all)
 
     normalize_somatic_tumor_normal_group = normalize_somatic_tumor_normal(
         batch_id=get_analysis_ids_related_batch_task,
--- dags/lib/tasks/clinical.py
+++ dags/lib/tasks/clinical.py
@@ -2,21 +2,26 @@ from typing import List, Optional, Set
 
 from airflow.decorators import task
 from lib.datasets import enriched_clinical
+from lib.utils import SKIP_EXIT_CODE
+from lib.utils_etl import BioinfoAnalysisCode
+
 from pandas import DataFrame
 
 
-@task.virtualenv(task_id='get_all_analysis_ids', requirements=["deltalake===0.24.0"], inlets=[enriched_clinical])
+@task.virtualenv(skip_on_exit_code=SKIP_EXIT_CODE, task_id='get_all_analysis_ids', requirements=["deltalake===0.24.0"], inlets=[enriched_clinical])
 def get_all_analysis_ids(analysis_ids: Optional[Set[str]] = None, sequencing_ids: Optional[Set[str]] = None, batch_ids: Optional[Set[str]] = None, batch_id: Optional[str] = None, skip: bool = False) -> List[str]:
-    
-    if skip:
-        return []
-    
+
     """
     Retrieves all analysis IDs for every sequencing IDs
     """
+    import sys
     from airflow.exceptions import AirflowFailException
     from lib.datasets import enriched_clinical
+    from lib.utils import SKIP_EXIT_CODE
     from lib.utils_etl_tables import get_analysis_ids, to_pandas
+
+    if skip:
+        sys.exit(SKIP_EXIT_CODE)
 
     batch_ids = batch_ids if batch_ids else [batch_id] if batch_id and batch_id != "" else []
 
@@ -29,34 +34,37 @@ def get_all_analysis_ids(analysis_ids: Optional[Set[str]] = None, sequencing_ids
 
     return all_analysis_ids
 
-@task.virtualenv(task_id='get_analysis_ids_related_batch', requirements=["deltalake===0.24.0"], inlets=[enriched_clinical])
-def get_analysis_ids_related_batch(analysis_ids: Optional[Set[str]], batch_id: str, skip: bool = False) -> str:
+
+@task.virtualenv(skip_on_exit_code=SKIP_EXIT_CODE, task_id='get_analysis_ids_related_batch', requirements=["deltalake===0.24.0"], inlets=[enriched_clinical])
+def get_analysis_ids_related_batch(analysis_ids: Optional[Set[str]], batch_id: str, bioinfo_analysis_code: BioinfoAnalysisCode, skip: bool = False) -> str:
 
     '''
     Utils function to revert to Batch ID a list of analysis IDs provided to a DAG as param.
     Will probably be mostly used in etl_ingest as long as we have ETLs using batch_id as input.
     '''
-    
+
     if batch_id and len(batch_id) > 0:
         return batch_id
-    
-    if skip:
-        return ""
-    
-    if not analysis_ids or len(analysis_ids) == 0:
-        raise AirflowFailException(f"No batch ID or analysis IDs were provided.")
-    
+
+    import sys
     from airflow.exceptions import AirflowFailException
     from lib.datasets import enriched_clinical
+    from lib.utils import SKIP_EXIT_CODE
     from lib.utils_etl_tables import get_batch_ids, to_pandas
 
+    if skip:
+        sys.exit(SKIP_EXIT_CODE)
+
+    if not analysis_ids or len(analysis_ids) == 0:
+        raise AirflowFailException("No batch ID or analysis IDs were provided.")
+
     df: DataFrame = to_pandas(enriched_clinical.uri)
-    clinical_df = df[["analysis_id", "sequencing_id", "batch_id"]]
-    all_batch_ids = sorted(get_batch_ids(clinical_df, analysis_ids=analysis_ids))
+    clinical_df = df[["analysis_id", "sequencing_id", "batch_id", "bioinfo_analysis_code"]]
+    all_batch_ids = sorted(get_batch_ids(clinical_df, bioinfo_analysis_code=bioinfo_analysis_code, analysis_ids=analysis_ids))
 
     if not all_batch_ids or len(all_batch_ids) == 0:
         raise AirflowFailException(f"No batch IDs found for the provided analysis IDs ({analysis_ids}).")
-    
+
     if len(all_batch_ids) > 1:
         raise AirflowFailException(f"Analysis IDs belong to more than one batch ID ({all_batch_ids}).")
 
