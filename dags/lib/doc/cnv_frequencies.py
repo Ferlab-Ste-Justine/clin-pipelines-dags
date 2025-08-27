3ffab3b0aa65e30c922cdc6423fcbb941bfb0760
Laura Bégin <laura.begin.hsj@ssss.gouv.qc.ca>
2025-08-27 16:33:08 -0400

    feat: CLIN-3816 Update cnv_frequencies doc

--- dags/lib/doc/cnv_frequencies.py
+++ dags/lib/doc/cnv_frequencies.py
@@ -1,22 +1,26 @@
 cnv_frequencies = """
 ### ETL CNV Frequencies DAG
 
-The **etl_cnv_frequencies** DAG enriches CNVs with frequencies computed using Nextflow svclustering job.
+The **etl_cnv_frequencies** DAG enriches CNVs with frequencies computed using Nextflow SVClustering.
 
 #### DAG Workflow
 
 1. **params_validate**: Verifies that `color` parameter was passed to the DAG in QA environment.
-1. **prepare_svclustering**: Prepares the CSV input file for the SVClustering process.
-1. **run_svclustering**: Executes the SVClustering Nextflow pipeline.
-1. **normalize_svclustering**: Normalizes the clustering output into a Delta table and computes frequencies for each cluster in the table.
+1. **ingest_fhir** group: Exports, normalizes, and enriches FHIR data.
+1. **prepare_svclustering**: Prepares the CSV input files for Nextflow: one for germline and one for somatic CNVs.
+1. **run_svclustering**: Executes the SVClustering Nextflow pipeline: once for germline and once for somatic CNVs.
+1. **normalize_svclustering**: Normalizes the clustering outputs into Delta tables and computes frequencies for each cluster.
 1. **enrich_cnv**: Enriches CNVs with frequencies and parental origin.
 1. **prepare_cnv_centric**: Prepares CNV for indexation into a `cnv_centric` Parquet table. 
 1. **qa** group: performs several quality checks:
-    - **no_dup_nextflow_svclustering**: Ensures no duplicates in the Nextflow SVClustering normalized Delta table.
+    - **no_dup_nextflow_svclustering_germline_task**: Ensures no duplicates in the Nextflow SVClustering Germline normalized Delta table.
+    - **no_dup_nextflow_svclustering_somatic_task**: Ensures no duplicates in the Nextflow SVClustering Somatic normalized Delta table.
     - **no_dup_nextflow_svclustering_parental_origin**: Ensures no duplicates in Nextflow SVClustering parental origin normalized Delta table.
     - **no_dup_cnv_centric**: Confirms the CNV centric data is deduplicated before indexing.
 1. **index_cnv_centric**: Indexes the `cnv_centric` table in Elasticsearch.
 1. **publish_cnv_centric**: Publishes the `cnv_centric` index, making it available for production use. On successful completion, a Slack notification is sent to notify the team.
+1. **delete_previous_release**: Deletes the previous release of the `cnv_centric` index to conserve storage.
+1. **rolling**: Triggers the `etl_rolling` DAG to swap indices and restart Arranger.
 
 #### Parameters
 
