4997a265d2c95c8b5d07b389bdffab163c4d8607
Laura Bégin <laurabegin1@gmail.com>
2025-09-03 11:37:11 -0400

    Revert "feat: CLIN-5054 Add unit tests for batch_folder_exists() + minio clea…"
    This reverts commit 1272cc57df2c37d015534f19778e6574836b073a.

--- tests/lib/test_utils_etl.py
+++ tests/lib/test_utils_etl.py
@@ -51,21 +51,3 @@ def test_skip_import_no_batch_ids():
     rendered = template.render(params=params)
 
     assert rendered == 'yes'
-
-
-def test_batch_folder_exists_true(clin_minio, clean_up_clin_minio):
-    from dags.lib.utils_etl import batch_folder_exists
-    from lib.config import clin_import_bucket
-
-    batch_id = "BATCH_1"
-    vcf_path = f"{batch_id}/1.case.hard-filtered.formatted.norm.VEP.vcf.gz"
-    clin_minio.load_string("{}", key=vcf_path, bucket_name=clin_import_bucket)
-
-    assert batch_folder_exists(clin_minio, batch_id) is True
-
-
-def test_batch_folder_exists_false(clin_minio, clean_up_clin_minio):
-    from dags.lib.utils_etl import batch_folder_exists
-
-    batch_id = "BATCH_1"
-    assert batch_folder_exists(clin_minio, batch_id) is False
