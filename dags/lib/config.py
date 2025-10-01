e7d91f27347507ad2e8c1caf5e5951fe1fa3e876
Yann MARCOU <creativeyann17@gmail.com>
2025-10-01 10:38:48 -0400

    feat: CLIN-5201 - release

--- dags/lib/config.py
+++ dags/lib/config.py
@@ -99,7 +99,7 @@ elif env == Env.QA:
         '4_data_to_import_somatic_normal',
     ]
 elif env == Env.STAGING:
-    fhir_image = 'ferlabcrsj/clin-fhir:148f1ce'
+    fhir_image = 'ferlabcrsj/clin-fhir:054ae71'
     pipeline_image = 'ferlabcrsj/clin-pipelines:8e2ce95'
     panels_image = 'ferlabcrsj/clin-panels:13b8182d493658f2c6e0583bc275ba26967667ab-1683653903'
     es_url = 'http://elasticsearch:9200'
@@ -113,7 +113,7 @@ elif env == Env.STAGING:
     franklin_assay_id = '2765500d-8728-4830-94b5-269c306dbe71'
     batch_ids = [] # automatic dags/lib/tasks/batch_type:get_all_batch_ids
 elif env == Env.PROD:
-    fhir_image = 'ferlabcrsj/clin-fhir:148f1ce'
+    fhir_image = 'ferlabcrsj/clin-fhir:054ae71'
     pipeline_image = 'ferlabcrsj/clin-pipelines:8e2ce95'
     panels_image = 'ferlabcrsj/clin-panels:13b8182d493658f2c6e0583bc275ba26967667ab-1683653903'
     es_url = 'https://workers.search.cqgc.hsj.rtss.qc.ca:9200'
