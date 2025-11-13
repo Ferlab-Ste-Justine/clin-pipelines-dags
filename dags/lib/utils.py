76be2a90e7dce392f5d6baf81a04ea9f25140821
Yann MARCOU <creativeyann17@gmail.com>
2025-11-13 16:38:50 -0500

    feat: CLIN-5201 - no verify

--- dags/lib/utils.py
+++ dags/lib/utils.py
@@ -23,7 +23,7 @@ def http_get(url: str, headers: Any = None) -> requests.Response:
 
 
 def http_get_file(url: str, path: str, headers: Any = None, chunk_size: int = 8192, md5: str = None, **kwargs) -> None:
-    with requests.get(url, headers=headers, stream=True, **kwargs) as response:
+    with requests.get(url, headers=headers, stream=True, verify=False, **kwargs) as response:
         response.raise_for_status()
         with open(path, 'wb') as file:
             for chunk in response.iter_content(chunk_size):
