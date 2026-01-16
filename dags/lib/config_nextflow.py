25d51b8045a37204b0a8962dcf7b72aa2035d7bf
georgette-femerling <georgette.femerling@gmail.com>
2026-01-16 13:09:16 -0600

    feat: CLIN-5543 bump post_processing_pipeline revision

--- dags/lib/config_nextflow.py
+++ dags/lib/config_nextflow.py
@@ -44,7 +44,7 @@ nextflow_svclustering_revision = 'v1.3.4-clin'
 
 nextflow_svclustering_parental_origin_revision = 'v1.1.3-clin'
 
-nextflow_post_processing_revision = 'v2.8.0'
+nextflow_post_processing_revision = 'v2.10.0'
 nextflow_cnv_post_processing_revision = 'v1.0.0'
 
 
