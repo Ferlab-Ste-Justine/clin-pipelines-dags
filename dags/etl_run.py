622554eff8d3dea6feaf3c2e97c94a6d8e2f2384
Yann MARCOU <creativeyann17@gmail.com>
2026-02-03 15:12:38 -0500

    feat: CLIN-4250 - reviews

--- dags/etl_run.py
+++ dags/etl_run.py
@@ -1,14 +1,12 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.decorators import task
 from airflow.models.param import Param
 from airflow.operators.empty import EmptyOperator
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.utils.trigger_rule import TriggerRule
-from lib.config import clin_datalake_bucket, etl_run_pending_folder, s3_conn_id
 from lib.slack import Slack
 from lib.tasks.params import get_sequencing_ids
+from lib.tasks.run_sequencings import save_sequencing_ids_to_s3
 
 with DAG(
         dag_id='etl_run',
@@ -31,25 +29,6 @@ with DAG(
         task_id="start",
         on_success_callback=Slack.notify_dag_start
     )
-
-    @task(task_id='save_sequencing_ids_to_s3')
-    def save_sequencing_ids_to_s3(sequencing_ids: list) -> str:
-        """
-        Save sequencing IDs to S3 in the .etl-run folder for later processing by etl_run_pending.
-        Each sequencing ID is saved as a separate file named with the sequencing ID.
-        """
-        if not sequencing_ids or len(sequencing_ids) == 0:
-            return "No sequencing IDs to save"
-        
-        s3 = S3Hook(s3_conn_id)
-
-        for seq_id in sequencing_ids:
-            # Create a file for each sequencing ID in the .etl-run folder
-            s3_key = f'{etl_run_pending_folder}/{seq_id}.txt'
-            # in the future we might want to save more info in the file, for now just create an empty file
-            s3.load_string("", s3_key, clin_datalake_bucket, replace=True)
-        
-        return f"Saved {len(sequencing_ids)} sequencing IDs to S3 in .etl-run/"
     
     save_task = save_sequencing_ids_to_s3(get_sequencing_ids())
 
--- dags/etl_run_release.py
+++ dags/etl_run_release.py
@@ -4,16 +4,17 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.models.param import Param
 from airflow.operators.empty import EmptyOperator
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.utils.trigger_rule import TriggerRule
-from lib.config import Env, env, clin_datalake_bucket, etl_run_pending_folder, s3_conn_id
+from lib.config import Env, env
 from lib.groups.ingest.ingest_fhir import ingest_fhir
 from lib.groups.nextflow.nextflow_germline import nextflow_germline
 from lib.operators.trigger_dagrun import TriggerDagRunOperator
 from lib.slack import Slack
 from lib.tasks import batch_type
 from lib.tasks.clinical import get_all_analysis_ids
+from lib.tasks.params import get_sequencing_ids
 from lib.tasks.params_validate import validate_color
+from lib.tasks.run_sequencings import cleanup_pending_sequencing_ids, get_pending_sequencing_ids
 from lib.utils_etl import (color, get_germline_analysis_ids, spark_jar)
 
 with DAG(
@@ -22,6 +23,7 @@ with DAG(
         schedule=None,
         catchup=False,
         params={
+            'sequencing_ids': Param([], type=['null', 'array']),
             'color': Param('', type=['null', 'string']),
             'spark_jar': Param('', type=['null', 'string']),
         },
@@ -50,33 +52,9 @@ with DAG(
         skip_post_import=False,
         spark_jar=spark_jar()
     )
-
-    @task(task_id='get_pending_sequencing_ids')
-    def get_pending_sequencing_ids() -> list:
-        """
-        Read sequencing IDs from all .txt filenames in the .etl-run folder on S3.
-        """
-        s3 = S3Hook(s3_conn_id)
-        
-        # List all files in the .etl-run folder
-        keys = s3.list_keys(bucket_name=clin_datalake_bucket, prefix=f'{etl_run_pending_folder}/')
-        
-        if not keys:
-            return []
-        
-        # Filter only .txt files and extract sequencing IDs from filenames
-        sequencing_ids = []
-        for key in keys:
-            if key.endswith('.txt'):
-                # Extract sequencing ID from filename: remove prefix and .txt extension
-                filename = key.replace(f'{etl_run_pending_folder}/', '').replace('.txt', '')
-                if filename:
-                    sequencing_ids.append(filename)
-        
-        return sequencing_ids
     
-    get_sequencing_ids_task = get_pending_sequencing_ids()
-    get_all_analysis_ids_task = get_all_analysis_ids(sequencing_ids=get_sequencing_ids_task)
+    param_sequencing_ids = get_sequencing_ids()
+    get_all_analysis_ids_task = get_all_analysis_ids(sequencing_ids=param_sequencing_ids)
 
     detect_batch_types_task = batch_type.detect(analysis_ids=get_all_analysis_ids_task, allowMultipleIdentifierTypes=True)
 
@@ -102,25 +80,8 @@ with DAG(
             'spark_jar': spark_jar(),
         }
     )
-
-    @task(task_id='cleanup_pending_sequencing_ids')
-    def cleanup_pending_sequencing_ids(sequencing_ids: list) -> str:
-        """
-        Delete only the .txt files from the .etl-run folder that were processed in this run.
-        """
-        if not sequencing_ids or len(sequencing_ids) == 0:
-            return "No files to delete"
-        
-        s3 = S3Hook(s3_conn_id)
-        
-        for seq_id in sequencing_ids:
-            # Reconstruct the filename for each processed sequencing ID
-            s3_key = f'{etl_run_pending_folder}/{seq_id}.txt'
-            s3.delete_objects(bucket=clin_datalake_bucket, keys=s3_key)
-           
-        return f"Deleted {sequencing_ids} processed files from S3"
     
-    cleanup_task = cleanup_pending_sequencing_ids(get_sequencing_ids_task)
+    cleanup_task = cleanup_pending_sequencing_ids(param_sequencing_ids)
 
     trigger_etl = TriggerDagRunOperator(
         task_id='trigger_etl',
@@ -142,7 +103,7 @@ with DAG(
     (
         start >> params_validate_color >>
         ingest_fhir_group >>
-        get_sequencing_ids_task >> get_all_analysis_ids_task >>
+        param_sequencing_ids >> get_all_analysis_ids_task >>
         detect_batch_types_task >>
         get_germline_analysis_ids_task >> nextflow_germline_task_group >>
         check_should_skip_franklin_task >> trigger_franklin_by_analysis_id_dags >>
