0001e806329e79f7c6cfbea801f6bdbc14a4b608
Yann MARCOU <creativeyann17@gmail.com>
2026-02-06 11:49:06 -0500

    feat: CLIN-9999 release

--- dags/lib/tasks/enrich.py
+++ dags/lib/tasks/enrich.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from lib.config import Env, chromosomes_2, env
+from lib.config import Env, chromosomes_4, env
 from lib.operators.spark_etl import SparkETLOperator
 from lib.utils_etl import ClinAnalysis
 
@@ -96,7 +96,7 @@ def variants(steps: str = 'initial', spark_jar: str = '', task_id: str = 'varian
             skip=skip,
             max_active_tis_per_dag=1,  # concurrent OverWritePartition, set to 1 if issues arise
             **kwargs
-        ).expand(chromosome=chromosomes_2)
+        ).expand(chromosome=chromosomes_4)
     else:
         return SparkETLOperator(
             entrypoint='variants',
