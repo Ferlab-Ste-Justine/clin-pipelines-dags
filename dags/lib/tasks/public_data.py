8e6578ba3d036e98226585b70ca4d7714a0a8e3e
Yann MARCOU <creativeyann17@gmail.com>
2026-04-02 15:46:16 -0400

    fix: CLIN-5828 - dont override is_new_version if true

--- dags/lib/tasks/public_data.py
+++ dags/lib/tasks/public_data.py
@@ -174,7 +174,8 @@ class PublicSourceDag:
         current_version = self.get_current_version()
         logging.info(f'current version: {current_version}')
         logging.info(f'last version: {self.last_version}')
-        self.is_new_version = current_version != self.last_version
+        if not self.is_new_version:
+            self.is_new_version = current_version != self.last_version
         logging.info(f'The file has a new version!' if self.is_new_version else f'The file is up to date!')
         return self.is_new_version
     
@@ -259,7 +260,7 @@ def update_public_data_info(dag_data: PublicSourceDag, **context):
 def should_continue(dag_data: PublicSourceDag):
     def check_is_new_version(dag_data: PublicSourceDag, **context):
         dont_skip = context["params"]["skip_if_not_new_version"] == 'no'
-        is_new_version = dag_data.is_new_version or dag_data.check_is_new_version()
+        is_new_version = dag_data.check_is_new_version()
         if not is_new_version and dont_skip:
             logging.info("skip_if_not_new_version is set to 'no', continuing the DAG even if the version is not new")
         return dont_skip or is_new_version
